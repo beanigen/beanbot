bc91ba3d6ac549891be1bb5fe4a3765803b9f011
BeanProto <92618727+BeanProto@users.noreply.github.com>
2022-05-02 09:49:56 +0800

    added add command

--- index.ts
+++ index.ts
@@ -11,14 +11,68 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log("Ready!")
+
+    const guildId = '935873409278165033'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+            name: 'num1',
+            description: 'The first number.',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+        },
+        {
+            name: 'num2',
+            description: 'The second number.',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+        }
+    ]
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.reply({
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+
+    const { commandName, options } = interaction
+
+    if (commandName === 'ping') {
+        interaction.reply({
             content: 'pong',
+            ephemeral: true,
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        interaction.reply({
+            content: `The sum is ${num1 + num2}`,
+            ephemeral: true,
         })
     }
-} )
+})
+
+
+
+
+
 
 client.login(process.env.TOKEN)
