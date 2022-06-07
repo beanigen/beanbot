0af6baaafd0a01ce54e3fc7ad4b87014623bd989
BeanProto <92618727+BeanProto@users.noreply.github.com>
2022-06-07 09:37:57 +0800

    somewhat finished the command

--- commands/user-info.ts
+++ commands/user-info.ts
@@ -2,8 +2,10 @@ import {ICommand} from "wokcommands";
 import DiscordJS, {
     GuildMember,
     MessageEmbed,
-    Interaction, User
+    Interaction, 
+    User
 } from "discord.js";
+
 
 export default {
     category: 'Testing',
@@ -20,13 +22,25 @@ export default {
     }],
     
     callback: async ({args, message, interaction: msgInt}) => {
+        
         const userToGet = message ? message.mentions.members?.first() : msgInt.options.getMember('user') as GuildMember
         //literally unused because minimum arguments is set to one lmfao
         if (!userToGet) {
             return 'Please specify a user to get info of'
         }
         
+       
         
+        
+        const embed = new MessageEmbed()
+            .setTitle('Info of ' + userToGet.user.tag)
+            .setColor('#FFFF00')
+            //this code is absolutely spaghetti, what do you expect from me?
+            // .setDescription('Join date: ' + userToGet.joinedAt + '\n Nickname: ' + userToGet.nickname + '\n ID: ' + userToGet.id + '\n Creation date: ' + userToGet.user.createdAt)
+            .setDescription(`Join date: ${userToGet.joinedAt} \n Creation date: ${userToGet.user.createdAt} \n Nickname: ${userToGet.nickname} \n ID: ${userToGet.id}`)
+            .setThumbnail(userToGet.displayAvatarURL({size: 96, dynamic: true}))
+        return embed
+
     }
 
     
